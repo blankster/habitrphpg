9afdd07a1f81a6b88bff116f18f956d38c958e79
blankster <blankster@users.noreply.github.com>
2014-11-28 04:08:47 +0100

    Recommit
    Sry this is my first comit on GitHub ever. I hope now is it as it should.

--- HabitRPHPG.php
+++ HabitRPHPG.php
@@ -75,7 +75,12 @@ class HabitRPHPG {
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $custom_headers);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // ignorates certificate error
 		$response = curl_exec($ch);
+
+        // output error message if an error is occured
+        if($response === false) die(curl_error($ch));
+
 		curl_close($ch);
 
 		if($this->options['debug']) {
@@ -157,7 +162,7 @@ class HabitRPHPG {
 	}
 
 	function updateTask($task_id, $data) {
-		return $this->_request("put", "user/task/$task_id", $data);	
+		return $this->_request("put", "user/task/$task_id", $data);
 	}
 
 	/**
